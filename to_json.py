8dad522061545e4c3e2127bd29453c60107374d3
Aaron <A_rothenberg@hotmail.com>
2014-04-15 16:32:23 -0500

    to_json add

---
+++ to_json.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+import json
+import unicodecsv
+
+input_file = unicodecsv.DictReader(open("/home/aaron/pycon/ohio_down/test/All_CAN.csv"), encoding='latin1')
+
+lst = []
+
+for (x, d) in enumerate(input_file):
+    d['id']=x
+    lst.append(d)
+
+for d in lst:
+    with open('/home/aaron/pycon/ohio_down/json_files/' + str(d['id']) + '.json', 'w') as outfile:
+        json.dump(d, outfile)

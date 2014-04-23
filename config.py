c527b9f006693c5775a192e21cd99ed4b66da5c1
Aaron <A_rothenberg@hotmail.com>
2014-04-22 19:09:45 -0500

    changed config file

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 
-download_dir =  '/path/to/download/directory'
+download_dir =  'path/to/dowloads/directory' #this is where the zip/exe files on the FTP sire are dowloaded to.
 
-data_dir =  '/path/to/data/directory'
+unzipped_dir =  'path/to/unzipped/dowloads' #you might want to make this a sub-directory of download_dir(see above)
 
+data_dir =  '/path/to/data/directory' #This will contain the json files extraced from the ftp downloads.
 
